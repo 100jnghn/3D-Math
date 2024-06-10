83d2d5a67d5c841105467be348b1cb5d450ab898
100jnghn <109127217+100jnghn@users.noreply.github.com>
2024-06-10 17:55:18 +0900

    FEAT : 점 수정 기능 추가
    [생성된 점 클릭]
    이름, 좌표 입력하고 버튼 누르면
    점의 정보가 수정, 반영된다

--- Assets/Scripts/DotController.cs
+++ Assets/Scripts/DotController.cs
@@ -5,18 +5,20 @@ using TMPro;
 
 public class DotController : MonoBehaviour
 {
+    private DotManager dotManager;
+
     public string dotName;                  // 점의 이름
     public TextMeshProUGUI positionText;    // UI에 들어갈 텍스트: A(0,0,0)
 
 
     void Start()
     {
-        
+        dotManager = GameObject.Find("DotManager").GetComponent<DotManager>();
     }
 
     void Update()
     {
-        
+        detectTouch();
     }
 
     // 점의 이름 설정
@@ -26,7 +28,7 @@ public class DotController : MonoBehaviour
     }
 
     // 사용자 input으로 position 입력받음. z값은 여기서 부호 반전
-    public void setPosition(int x, int y, int z)
+    public void setPosition(float x, float y, float z)
     {
         Vector3 pos = new Vector3(x, y, z * -1);
         transform.position = pos;
@@ -35,6 +37,33 @@ public class DotController : MonoBehaviour
     // UI에 들어갈 텍스트 설정. A(0,0,0)
     public void setText()
     {
-        positionText.text = dotName + "(" + transform.position.x + transform.position.y + transform.position.z * -1 + ")";
+        positionText.text = dotName + "(" + transform.position.x + " " + transform.position.y + " " + transform.position.z * -1 + ")";
     }
+
+    void detectTouch()
+    {
+        // 터치가 하나 이상 발생
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);    // 첫 번째 터치 가져옴
+
+            // 터치가 시작된 순간
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == this.transform)
+                    {
+                        dotManager.selectedDot = gameObject;
+                        dotManager.changeUI();
+                    }
+                }
+            }
+        }
+    }
+
+    
 }
